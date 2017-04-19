0e94da87a3fe026239ac3c4be759c69564d09888
Rebecca Wilkerson <rueTH@users.noreply.github.com>
2017-04-19 12:45:17 -0500

    i promished you a push! here i've jumbled everything around again. Still working. I think i just backtracked since we spoke, though?

--- js/main.js
+++ js/main.js
@@ -1,2 +1,40 @@
-"use strict";
+// "use strict";
 
+var pineTree = {};
+
+
+
+
+//add event listeners & fix C (constant/trunk/whatever)
+
+// //---button click?
+// btn.onclick = tree();
+
+
+// //---keydown return
+// onkeydown = function(returnEvent){
+//   if(returnEvent.which == 13);
+//     tree();
+//   }
+
+function tree(taco){
+  var output = "";
+  var branch = document.getElementById('char').value;
+  var height = document.getElementById('height').value;
+  var x = height - 1;
+  var C = pineTree.height; //blerg this isn't right. 
+  var space = "\n";
+  var btn = document.getElementById('btn');
+
+  btn.onclick = console.log(output, {    
+      if( !(taco.height) || !(taco.char) ) {
+        alert("both fields must contain a value")
+      } else{
+        for (i = 0; i < taco.height; i++) {
+          pineTree += space.repeat(x) + taco.branch.repeat(i);
+          pineTree += taco.C + taco.branch.repeat(i);
+          pineTree += '<br>';
+        }
+      }
+  })
+}
